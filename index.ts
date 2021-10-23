9620b257c04098458d93ca0c60d0543d44df2b39
Leo Shen <343206898@qq.com>
2021-10-23 18:56:06 +0800

    Feat: 读取index.html

--- index.ts
+++ index.ts
@@ -1,13 +1,37 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
+import * as fs from "fs";
+import * as path from "path";
+import * as url from "url";
 
-const server =  http.createServer()
+const server = http.createServer()
 
-server.on('request',(request:IncomingMessage,response: ServerResponse) => {
-    console.log(request.method,'request.method')
-    console.log(request.url,'request.url')
-    console.log(request.headers,'request.headers')
-    response.end('hi')
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+    const {method, headers} = request
+    const obj = url.parse(request.url)
+    console.log(obj)
+    switch (request.url) {
+        case '/index.html':
+            fs.readFile(path.resolve(__dirname, 'public', 'index.html'), (err, data) => {
+                if (err) throw err
+                response.end(data.toString())
+            })
+            break;
+        case '/style.css':
+            response.setHeader('Content-Type', 'text/css;charset=utf-8')
+            fs.readFile(path.resolve(__dirname, 'public', 'style.css'), (err, data) => {
+                if (err) throw err
+                response.end(data.toString())
+            })
+            break;
+        case '/main.js':
+            response.setHeader('Content-Type','text/javascript;charset=utf-8')
+            fs.readFile(path.resolve(__dirname, 'public', 'main.js'), (err, data) => {
+                if (err) throw err
+                response.end(data.toString())
+            })
+            break;
+    }
 })
 
 server.listen(8888)
